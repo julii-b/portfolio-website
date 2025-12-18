f07761edac60db9c0509042ae25ac411be4ff10d
Julius Busch <117680351+julii-b@users.noreply.github.com>
2025-12-18 13:43:52 +0100

    feat: add non-functional contact form

--- app/components/contact-me/contact-me.tsx
+++ app/components/contact-me/contact-me.tsx
@@ -1,17 +1,69 @@
+'use client";'
+
 import Form from "next/form";
 import styles from "./contact-me.module.css";
+import Script from "next/dist/client/script";
+import { Button } from "@/app/ui/button/button";
+import { width } from "@fortawesome/free-brands-svg-icons/fa11ty";
+import { Input, TextArea } from "@/app/ui/input/input";
 
 
 export default function ContactMe() {
   return (
     <section className={styles.section} id="contact-me" key="contact-me-section">
 
+      <Script
+        src="https://challenges.cloudflare.com/turnstile/v0/api.js"
+        async
+        defer
+      ></Script>
+
       <h2>Contact Me</h2>
 
-      <Form action="/">
-        <input />
-        <textarea />
-        <button type="submit">Send</button>
+      <Form
+      action="/"
+      className={styles.form}
+      > 
+        <div className={styles.nameWrapper}>
+          <label htmlFor="name">Name</label>
+          <Input id="name" />
+        </div>
+
+        <div className={styles.emailWrapper}>
+          <label htmlFor="email">Email</label>
+          <Input id="email" type="email" />
+        </div>
+
+        <div className={styles.messageWrapper}>
+          <label htmlFor="message">Message</label>
+          <TextArea id="message" />
+        </div>
+
+        <div className={styles.captchaAndSubmitWrapper}>
+
+          <div className={styles.captchaWrapper}>
+            {/* sitekey for testing purposes:
+            always okay: 1x00000000000000000000AA
+            always fail: 2x00000000000000000000AB
+            force interaction: 3x00000000000000000000FF
+            */}
+            <div
+            className="cf-turnstile"
+            data-sitekey="1x00000000000000000000AA"
+            data-theme="light"
+            data-size="flexible"
+            ></div>
+          </div>
+
+          <Button
+          type="submit"
+          className={styles.submitButton}
+          >
+            Send
+          </Button>
+
+        </div>
+
       </Form>
 
     </section>
