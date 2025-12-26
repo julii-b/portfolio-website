1583dd84c7969812e125095717fa32447411ddc7
Julius Busch <117680351+julii-b@users.noreply.github.com>
2025-12-26 20:43:05 +0100

    refactor: move content of sections into seperate file and export sections all from the same file

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
-import Projects from "@/app/components/sections/projects";
-import Education from "@/app/components/sections/education";
 import Header from "@/app/components/header/header";
-import WorkExperience from "@/app/components/sections/work-experience";
-import Languages from "@/app/components/sections/languages";
-import ContactMe from "@/app/components/contact-me/contact-me";
+import { Projects, Education, WorkExperience, Languages, ContactMe } from "@/app/components/sections/sections";
 
 export default function Home() {
   return (
---
+++ app/components/navigation-chat/navigation-chat.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import styles from "./navigation-chat.module.css";
+import Link from "next/link";
+import { generateChatResponse, ChatResponse } from "./chat";
+import { Input } from "@/app/ui/input/input";
+import { useState } from "react";
+import { redirect } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
+export default function NavigationChat() {
+
+  const [response, setResponse] = useState<ChatResponse | undefined>();
+
+  const debouncedOnChange = useDebouncedCallback(async (userPrompt: string) => {
+    console.log("User prompt:", userPrompt);
+    const response = await generateChatResponse(userPrompt);
+    setResponse(response);
+    if (response.function_name === "scroll_to_section" && response.parameters?.section_name) {
+      const section = response.parameters.section_name;
+      redirect(`/#${section}`);
+    }
+  }, 1000);
+
+  return (
+    <nav className={styles.nav}>
+      {response && <p>{response.answer}</p> }
+
+      <Link href="/#projects">Projects</Link>
+      <Link href="/#education">Education</Link>
+      <Link href="/#work-experience">Work Experience</Link>
+      <Link href="/#languages">Languages</Link>
+      <Link href="/#contact-me">Contact Me</Link>
+
+      <Input onChange={async (e) => {
+        debouncedOnChange(e.target.value);
+      }} />
+    </nav>
+  );
+}
