381b68cecbde81fec3174d7447ab34f16f13629a
Julius Busch <117680351+julii-b@users.noreply.github.com>
2025-12-27 23:53:36 +0100

    feat: add text and background images to projects and work experience. Still not complete

--- app/components/sections/sections-content.tsx
+++ app/components/sections/sections-content.tsx
@@ -26,7 +26,10 @@ const sectionsContent: SectionContent[] = [
           {name: "TypeScript"},
         ],
         children: (
-          <p>My personal portfolio website showcasing my projects, skills, and experience. Built with Next.js and React.</p>
+          <>
+            <p>My personal portfolio website showcasing my projects, skills, and experience. Built with Next.js and React.</p>
+            <p>Once the project is finished, I will have to complete this description.</p>
+          </>
         ),
       },
       {
@@ -43,8 +46,48 @@ const sectionsContent: SectionContent[] = [
           {name: "React Router"},
           {name: "TypeScript"},
         ],
+        backgroundImageUrl: "/background-images/simplepolls-background.png",
         children: (
-          <p>alskdfjdk</p>
+          <>
+            <p>
+              <b>Frontend:</b> <a href="https://simplepolls.julius-busch.com" target="_blank" rel="noopener noreferrer"> simplepolls.julius-busch.com </a>
+            </p>
+            <p>
+              <b>Backend API Documentation:</b> <a href="https://apisimplepolls.julius-busch.com/docs" target="_blank" rel="noopener noreferrer"> apisimplepolls.julius-busch.com/docs </a>
+            </p>
+            <p>
+              <b>GitHub Repository:</b> <a href="https://github.com/julii-b/SimplePolls" target="_blank" rel="noopener noreferrer"> github.com/julii-b/SimplePolls </a>
+            </p>
+            <p>
+              With SimplePolls, I wanted to create a simple and user-friendly application that showcases my ability to build full-stack web applications.
+              <br />
+              The application has a React frontend and an Express backend.
+            </p>
+            <p>
+              For the backend I decided to create a public RESTful API.
+              <br />
+              The API is documented using OpenAPI (<a href="https://apisimplepolls.julius-busch.com/docs" target="_blank" rel="noopener noreferrer">Swagger UI</a>) and can be accessed publicly.
+              <br />
+              I used Prisma as an ORM to interact with a PostgreSQL database.
+              <br />
+              I implemented various security features such as rate limiting and authentication for modifying data, and made a conscious decision on how to set up CORS.
+              <br />
+              To make sure that everything works as expected, I wrote tests for the API using Supertest and Vitest.
+            </p>
+            <p>
+              For the <a href="https://simplepolls.julius-busch.com" target="_blank" rel="noopener noreferrer">frontend</a> I wanted to create a fun and intuitive interface.
+              It is built with React and uses React Router in Data Mode for routing.
+              <br />
+              The design is responsive, allowing users to enjoy the application on both mobile and desktop devices.
+              <br />
+              Users can create, edit, share and participate in polls.
+              <br />
+              Also, I implemented internationalization using i18next, making the frontend available in English, German and French.
+            </p>
+            <p>
+              If you want, please use the links above to check out the project and its code!
+            </p>
+          </>
         ),
       },
       {
@@ -56,8 +99,27 @@ const sectionsContent: SectionContent[] = [
           {name: "TypeScript"},
           {name: "3rd Party APIs"},
         ],
+        backgroundImageUrl: "/background-images/guesstheflag-background.png",
         children: (
-          <p>alskdfjdk</p>
+          <>
+            <p>
+              <b>Demo:</b> <a href="https://guesstheflag.julius-busch.com" target="_blank" rel="noopener noreferrer"> guesstheflag.julius-busch.com </a>
+            </p>
+            <p>
+                <b>GitHub Repository:</b> <a href="https://github.com/julii-b/guesstheflag" target="_blank" rel="noopener noreferrer"> github.com/julii-b/guesstheflag </a>
+            </p>
+            <p>
+              Because I love learning about different countries and their flags, I created GuessTheFlag to test and improve my flag knowledge in a fun way, and also practice my React skills.
+            </p>
+            <p>
+                You are shown a random flag and you have to guess the country name as quickly as possible, collecting points for correct answers and speed, while competing against your own high score.
+            </p>
+            <p>
+                Because I use the <a href="https://restcountries.com/" target="_blank" rel="noopener noreferrer">REST Countries API</a> to get the country names, the user can guess the country names in 26 different languages.
+                <br />
+                For the flags themselves, I use the <a href="https://flagcdn.com/" target="_blank" rel="noopener noreferrer">Flags API & CDN</a> by <a href="https://flagpedia.net/" target="_blank" rel="noopener noreferrer">Flagpedia.net</a>. This way, I can be sure that the country names and flags are always up to date.
+            </p>
+          </>
         ),
       },
     ],
@@ -145,8 +207,20 @@ const sectionsContent: SectionContent[] = [
         subtitle: "International German School of Brussels (iDSB)",
         location: "Wezembeek-Oppem, Belgium",
         time: "2025 - Present",
+        backgroundImageUrl: "/background-images/wezembeek-oppem.jpg",
         children: (
-          <p>kjhjhjk</p>
+          <>
+            <ul>
+              <li>IT support and troubleshooting for staff, students and parents in a school environment</li>
+              <li>User account management, device setup and system administration (laptops, access rights, school systems)</li>
+              <li>Preparation and configuration of devices and software for new staff members</li>
+              <li>Hardware troubleshooting and basic laptop repairs, including coordination of external repairs</li>
+              <li>Frontend development of internal Power Apps for document and process digitalization</li>
+              <li>Design and iteration of application frontends based on existing documents and user feedback</li>
+              <li>Development of small scripts and internal tools</li>
+              <li>Occasional research, procurement and setup of IT equipment for specific use cases</li>
+            </ul>
+          </>
         ),
       },
       {
@@ -156,7 +230,13 @@ const sectionsContent: SectionContent[] = [
         location: "Remote",
         time: "2024",
         children: (
-          <p>kjhjhjk</p>
+          <>
+            <ul>
+              <li>Designing complex coding problems to test the boundaries of current AIs</li>
+              <li>Creating detailed solutions and explanations for coding challenges</li>
+              <li>Contributed to AI training by solving coding challenges that current AIs cannot solve</li>
+            </ul>
+          </>
         ),
       },
       {
@@ -165,9 +245,15 @@ const sectionsContent: SectionContent[] = [
         subtitle: "VacuuBrand",
         location: "Wertheim, Germany",
         time: "2019",
+        skills: [
+          {name: "Web Design"},
+          {name: "UI Design"},
+          {name: "CSS"},
+          {name: "HTML"},
+        ],
         backgroundImageUrl: "https://cdn.pixabay.com/photo/2017/05/24/06/56/wertheim-2339708_1280.jpg",
         children: (
-          <p>kjhjhjk</p>
+          <p>Designing a landing page for a new product</p>
         ),
       }
     ],
