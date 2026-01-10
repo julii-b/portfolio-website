3022daef682f3c8a1db72d5f50c063e2ddc2ddaa
Julius Busch <117680351+julii-b@users.noreply.github.com>
2026-01-10 19:19:01 +0100

    feat: implement action function with email notification after submitting contact form

--- app/components/contact-me/contact-me.tsx
+++ app/components/contact-me/contact-me.tsx
@@ -6,12 +6,19 @@ import Script from "next/script";
 import { Button } from "@/app/ui/button/button";
 import { Input, TextArea } from "@/app/ui/input/input";
 import MetroStation from "@/app/components/metro-line/metro-station/metro-station";
+import action from "./action";
+import { useActionState, useState } from "react";
+
 
 /**
  * Contact Me section component - to be implemented.
  * @returns The rendered Contact Me section.
  */
 export default function ContactMe() {
+
+  // Initialize formState with chat history. formAction will update formState:
+  const [formState, formAction] = useActionState(action, {});  
+
   return (
     <section className={styles.section} id="contact-form" key="contact-form">
 
@@ -28,22 +35,35 @@ export default function ContactMe() {
       </div>
 
       <Form
-      action="/"
+      action={formAction}
       className={styles.form}
-      > 
+      >
+        <div className={styles.mainErrorMessage}>
+          {formState.errorMessage}
+        </div>
+
         <div className={styles.nameWrapper}>
           <label htmlFor="name">Name</label>
-          <Input id="name" />
+          <Input id="name" name="name" />
+          <div className={styles.errorMessage}>
+            {formState.fieldErrors?.name && formState.fieldErrors.name.join(", ")}
+          </div>
         </div>
 
         <div className={styles.emailWrapper}>
           <label htmlFor="email">Email</label>
-          <Input id="email" type="email" />
+          <Input id="email" type="email" name="email" />
+          <div className={styles.errorMessage}>
+            {formState.fieldErrors?.email && formState.fieldErrors.email.join(", ")}
+          </div>
         </div>
 
         <div className={styles.messageWrapper}>
           <label htmlFor="message">Message</label>
-          <TextArea id="message" />
+          <TextArea id="message" name="message" />
+          <div className={styles.errorMessage}>
+            {formState.fieldErrors?.message && formState.fieldErrors.message.join(", ")}
+          </div>
         </div>
 
         <div className={styles.captchaAndSubmitWrapper}>
@@ -60,7 +80,12 @@ export default function ContactMe() {
             data-theme="light"
             data-size="flexible"
             ></div>
+            <div className={styles.errorMessage}>
+            {/*formState.fieldErrors?.token && formState.fieldErrors.token.join(", ")*/}
           </div>
+          </div>
+          
+
 
           <Button
           type="submit"
