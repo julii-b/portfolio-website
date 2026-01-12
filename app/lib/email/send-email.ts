092efc61c5a48bf2d819740fcaa86bb013f733cc
Julius Busch <117680351+julii-b@users.noreply.github.com>
2026-01-12 22:33:11 +0100

    feat: Create html template for emails, move email related files into a folder

--- app/lib/email/send-email.ts
+++ app/lib/email/send-email.ts
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 import { env } from "process";
-import { sanitizeEmailHeader, sanitizeStringForHtml } from "./sanitize-operations";
+import { sanitizeEmailHeader, sanitizeStringForHtml } from "../sanitize-operations";
 
 
 export class MailError extends Error {
