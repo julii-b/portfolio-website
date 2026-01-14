c140a719d75b95e05ef53d299d86b8700e32ab90
Julius Busch <117680351+julii-b@users.noreply.github.com>
2026-01-14 20:47:59 +0100

    refactor: use .env variable for email in contact-form action

--- app/components/contact-me/action.ts
+++ app/components/contact-me/action.ts
@@ -68,7 +68,7 @@ export default async function action (
     await sendEmail({
       from: "contact-form@julius-busch.com",
       fromName: `Contact Form: ${name}`,
-      to: "mail@julius-busch.com",
+      to: env.CONTACT_EMAIL!,
       replyTo: email,
       subject: `New contact form message from ${name} - ${email}`,
       plainMessage: notificationMessageReceived(name, email, message).plainMessage,
