e8584d71024513b738cea438f785111099a2e291
Julius Busch <117680351+julii-b@users.noreply.github.com>
2026-01-18 16:40:18 +0100

    fix: comment out console log

--- app/lib/text-generation/generateChatResponse.ts
+++ app/lib/text-generation/generateChatResponse.ts
@@ -62,7 +62,7 @@ async function generateChatResponseRecursive(chatHistory: ChatHistoryEntry[], re
   
     // Parse the response text as JSON:
     if (response.text !== undefined) {
-      console.log(response.text);
+      // console.log(response.text);
       return JSON.parse(response.text.replace(endTokenModel, '')) as ChatResponse;
     } else {
       throw new Error("No response text");
